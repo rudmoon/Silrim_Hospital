aea2c04c85b980d31fdba10e0ebb3e3a11ae251a
rudmoon <96547605+rudmoon@users.noreply.github.com>
2023-03-18 08:19:08 +0900

    230318 commit
    이미지를 어떻게 딱 맞추지 사이트처럼? 그림크기 자체를 바꿔야하나?

---
+++ main.js
@@ -0,0 +1,15 @@
+// Header slide의 높이로써 #header, .header__slides 높이 주기
+
+// let header = document.querySelector('#header');
+// let slider = document.querySelector('.header__slider');
+// let slides = document.querySelectorAll('.slide');
+// let slideHeight = 0;
+
+// for(let i=0; i<slides.length; i++) {
+//     if(slideHeight < slides[i].clientHeight) {
+//         slideHeight = slides[i].clientHeight;
+//     }
+// }
+// console.log(slideHeight);
+// header.clientHeight = slideHeight;
+// console.log(header.clientHeight);
