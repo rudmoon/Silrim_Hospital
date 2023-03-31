ed5ab214e8240852420c17aac627706c44369f73
rudmoon <96547605+rudmoon@users.noreply.github.com>
2023-04-01 03:57:44 +0900

    230401_2
    230401_2

--- main.js
+++ main.js
@@ -58,7 +58,21 @@ function scrollFadeOut() {
     })
 }
 
+var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(37.56686, 126.97828), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
+    mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
+  };
 
+// 지도를 생성한다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+// 지도에 마커를 생성하고 표시한다
+var marker = new kakao.maps.Marker({
+  position: new kakao.maps.LatLng(37.56682, 126.97865), // 마커의 좌표
+  map: map, // 마커를 표시할 지도 객체
+});
 
 function init() {
     scrollFadeOut();
