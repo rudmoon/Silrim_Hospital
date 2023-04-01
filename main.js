1a4225b5cf2a2fcc8921ecc184bd19c397ea5112
rudmoon <96547605+rudmoon@users.noreply.github.com>
2023-04-01 12:48:45 +0900

    230401_5
    230401_5

--- main.js
+++ main.js
@@ -58,23 +58,70 @@ function scrollFadeOut() {
     })
 }
 
-var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-  mapOption = {
-    center: new kakao.maps.LatLng(37.49285318116372, 127.04204271354173), // 지도의 중심좌표
+function makingMap() {
+    var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+    mapOption = {
+    center: new kakao.maps.LatLng(37.49255318116372, 127.04244271354173), // 지도의 중심좌표
     level: 3, // 지도의 확대 레벨
     mapTypeId: kakao.maps.MapTypeId.ROADMAP, // 지도종류
-  };
+};
 
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 지도에 마커를 생성하고 표시한다
 var marker = new kakao.maps.Marker({
-  position: new kakao.maps.LatLng(37.49285318116372, 127.04204271354173), // 마커의 좌표
+  position: new kakao.maps.LatLng(37.49255318116372, 127.04244271354173), // 마커의 좌표
   map: map, // 마커를 표시할 지도 객체
 });
 
 marker.setMap(map);
+}
+
+// 모달창 구현시, 뒷배경 스크롤을 막아둠.
+function disableScroll() {
+    // 현재 스크롤 위치 저장
+let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+    // 스크롤 위치를 다시 설정하여 스크롤을 막음
+window.onscroll = function() {
+    window.scrollTo(scrollLeft, scrollTop);
+};
+}
+
+  // 스크롤 막기 해제
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+let modalButton = document.querySelector('.aside__doctors');
+let modal = document.querySelector('#modal');
+let modalXbutton = document.querySelector('.modal__xButton');
+modalButton.addEventListener('click',() => {
+    modal.style.display = 'flex';
+    disableScroll();
+});
+
+modalXbutton.addEventListener('click',() => {
+    modal.style.display = 'none';
+    enableScroll();
+})
+
+
+
+
+
+
+// Arrow_totheTop Button
+let Arrow_totheTop = document.querySelector('.aside__totheTop');
+Arrow_totheTop.addEventListener('click',() => {
+    window.scrollTo({
+        top : 0,
+        left : 0,
+        behavior: 'smooth',
+    })
+})
 
 function init() {
     scrollFadeOut();
